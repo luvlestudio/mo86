abbce4877dd9036a04680e670009d45d929ce1dc
luvlestudio <sowoojoo31@gmail.com>
2025-05-26 22:50:16 +0900

    6

--- gallery.js
+++ gallery.js
@@ -48,7 +48,7 @@ window.onload = () => {
       content: {
         title: "병호 ♥ 아연의 모바일 청첩장",
         description: "2025년 8월 30일 토요일♡",
-        imageUrl: "https://i.imgur.com/P1mroV6.png",
+        imageUrl: "https://i.imgur.com/X1DO5tJ.png",
         imageWidth: 1200,
         imageHeight: 630,
         link: {
